f0ad09587492c787a4bf38a019c29d9e17303c11
Dean Palm <dean@sxsw.com>
2017-11-05 20:06:39 -0600

    wet paint please don't touch

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,11 +1,21 @@
 class Blog
 
   attr_reader :entries
-  
+  attr_writer :post_source
+
   def initialize
     @entries = []
   end
 
+  def add_entry(entry)
+    entries << entry
+  end
+
+  def new_post(*args)
+    post_source.call(*args).tap do |p|
+      p.blog = self
+    end
+  end
 
   def title
     "Watching Paint Dry"
@@ -13,6 +23,12 @@ class Blog
 
   def subtitle
     "The trusted source for drying paint news & opinion"
+  end
+
+  private
+
+  def post_source
+    @post_source ||= Post.public_method(:new)
   end
 
 end
