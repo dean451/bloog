e5b150924c498b01aa13c4608c024332e61ba84d
Dean Palm <dean@sxsw.com>
2017-11-12 01:43:51 -0600

    publishable blog entries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :init_blog
+
+  private
+
+  def init_blog
+    @blog = THE_BLOG
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'blog/index'
 
 root to: "blog#index"
-
+resources :posts
 
 end
